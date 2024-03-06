d8a854b8b5062f2a7b20d85a9086a38a63ea7211
Tin Tin Trinh <tintin@mnemonic.no>
2024-03-06 15:32:27 +0100

    added ksat script quickstart

---
+++ spin_up_ksat_old_prod.sh
@@ -0,0 +1,12 @@
+docker_vars_filename="ksat_old_prod"
+sudo docker container ls -af label=docker_compose_file=compose_file_$docker_vars_filename
+
+ansible-playbook playbooks/1_deploy_linux_containers.yml --extra-var "docker_vars_file=$docker_vars_filename.yml" -vvv
+inventory="environments/development/inventory_$docker_vars_filename.ini"
+
+# docker restart policy: keep containers running
+docker update --restart unless-stopped $(docker container ls -af label=docker_compose_file=compose_file_$docker_vars_filename -q)
+
+
+### tear down commands
+#ansible-playbook playbooks/2_teardown_linux_containers.yml --extra-var "docker_vars_file=ksat_old_prod.yml"
