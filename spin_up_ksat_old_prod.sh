c5c40054c491764d082eed210ddeec183299eed8
Tin Tin Trinh <tintin.trinh@outlook.com>
2024-03-11 09:47:50 +0100

    remove verbose

--- spin_up_ksat_old_prod.sh
+++ spin_up_ksat_old_prod.sh
@@ -1,7 +1,7 @@
 docker_vars_filename="ksat_old_prod"
 sudo docker container ls -af label=docker_compose_file=compose_file_$docker_vars_filename
 
-ansible-playbook playbooks/1_deploy_linux_containers.yml --extra-var "docker_vars_file=$docker_vars_filename.yml" -vvv
+ansible-playbook playbooks/1_deploy_linux_containers.yml --extra-var "docker_vars_file=$docker_vars_filename.yml"
 inventory="environments/development/inventory_$docker_vars_filename.ini"
 
 # docker restart policy: keep containers running
